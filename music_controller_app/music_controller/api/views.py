7d3760c82b44300f301aa34596d53ede839c8077
Aquacat <b02605061@gmail.com>
2024-02-23 20:23:47 -0600

    add API view

--- music_controller_app/music_controller/api/views.py
+++ music_controller_app/music_controller/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("Hello Yinzi!")
+
+class RoomView (generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
